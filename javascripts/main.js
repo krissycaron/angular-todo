658257bf60a7211bc2f3426877e426149cb6e582
Kristin Caron <krissycaron@gmail.com>
2017-05-09 19:48:50 -0500

    added nav links

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,27 @@
-console.log("hi");
+//need to let the project know you will be using angular. this is "var becasue it needs to be global"
+//first thing it the name but then it will be an array of plug ins.
+var app = angular.module("TodoApp", []);
+
+//controller 
+//dynamically creating a nav bar (NEW iife!!!)
+//what goes in the (and the angular bits you want to use)
+app.controller("NavCtrl", ($scope)=> {
+	$scope.cat = "Meow";
+	$scope.navItems=[{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
+});
+
+
+//second controller - 
+//must pass in the scope to the variable 
+app.controller("ItemCtrl", ($scope) => {
+	$scope.dog="Woof!";
+
+});
+
+
+
+
+
+
+
+
